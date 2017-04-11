8d380f906db0bd107dd8e31fc4d51c1399740d6d
Robert Bojs <robert.bojs@gmail.com>
2017-04-11 14:12:58 +0200

    nya strukturen, pitchad för CHessIT

--- sql_interface.py
+++ sql_interface.py
@@ -1,6 +1,6 @@
 import pyodbc
 
-cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 10.0};SERVER=localhost;DATABASE=ED;UID=kth;PWD=pass')
+cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 10.0};SERVER=localhost;DATABASE=CustomerDATA;UID=kth;PWD=pass')
 
 cursor = cnxn.cursor()
 
@@ -9,7 +9,15 @@ sql_string_2 = "select Decision_Date, Quote_Total_In_Sys_Currency, Quote_Total_I
 sql_string_3 = "select Quote_Sub_Total, Quote_Total from dbo.SFA_Quote"
 sql_string_4 = "select Zip, City, Annual_Revenue, Number_Of_Employee, Annual_Number_of_Orders_Map, Decision_Date, Quote_Total_In_Sys_Currency, Quote_Total_In_Oppt_Currency, Pipeline_Stage from dbo.Company join dbo.SFA_Opportunity on dbo.Company.Company_Id = dbo.SFA_Opportunity.Company_Id join dbo.SFA_Quote on dbo.Company.Company_Id = dbo.SFA_Quote.Company_Id"
 foo ="select * from dbo.SFA_Opportunity"
-cursor.execute(sql_string_4)
+boo ='select [CustomerDATA].[dbo].[Contact].Company_Id FROM [CustomerDATA].[dbo].[Company]  join [CustomerDATA].[dbo].[Contact] on [CustomerDATA].[dbo].[Company].[Company_Id]=[CustomerDATA].[dbo].[Contact].Company_Id;'
+
+query = open('Queries.txt','r')
+
+loo = query.read()
+
+#print(loo)
+#customerQuery ='select a.Company_ID, a.Zip, a.Annual_Revenue, a.Number_Of_Employee,  from dbo.Company as a; '
+cursor.execute(loo)
 
 #dbo.Company (Zip, City, Annual_Revenue, Number_Of_Employee, Annual_Number_of_Orders_Map)
 #dbo.SFA.Opportunity (Decision_Date, Quote_Total_In_Sys_Currency, Quote_Total_In_Oppt_Currency, Status, Pipeline_Stage)
