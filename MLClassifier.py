9de9a92624741e4f926ce03ad25fa1b4b963ffdf
Robert Bojs <rbojs@kth.se>
2017-04-17 14:42:24 +0200

    kommunist

--- MLClassifier.py
+++ MLClassifier.py
@@ -1,16 +1,16 @@
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
-from SCBDB import *
+import SCBDB as SCBDB
 
 
 def naive_bayes():
     #training data
     X = np.array([[-1,-1],[-2,-1],[-3,-2],[1,1],[2,1],[3,2]])
-    kommunerlist = SCBDB.kommunToData('kommuner.txt')
-
+    kommunist = SCBDB.kommunToData('kommuner.txt')
+    print(kommunist)
     #classes
     Y = np.array([1,1,1,2,2,2])
-    
+
     clf = GaussianNB()
     GaussianNB(priors=None)
     clf.fit(X,Y)
