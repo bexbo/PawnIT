92b58492694ce0ee0076afd556f32d466703a14a
Benny Feng <bennyfeng96@hotmail.com>
2017-04-17 14:08:02 +0200

    added function for kommun to data

--- TreeClassifier.py
+++ TreeClassifier.py
@@ -1,2 +1,24 @@
-import SCBDB
+from SCBDB import SCBData
+
+
+def kommunToData(kommuner):
+    kommunlist = []
+    for kommun in kommuner:
+        kommun = kommun.strip('\n').strip(',').strip('"')
+        foo = SCBData(kommun)
+        kommunlist.append(foo)
+
+    for kommun in kommunlist:
+        print(kommun.featureList)
+
+    return kommunlist
+
+
+
+
+
+kommuner = open('kommuner.txt','r')
+kommuner = kommuner.readlines()
+
+kommunlist= kommunToData(kommuner)
 
