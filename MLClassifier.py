1c256d76a848505627bfa74de46c23f24c2c7151
Robert Bojs <rbojs@kth.se>
2017-04-17 16:14:06 +0200

    naivebayes classifier done

--- MLClassifier.py
+++ MLClassifier.py
@@ -5,21 +5,21 @@ import SCBDB as SCBDB
 
 def naive_bayes():
     #training data
-    X = np.array([[-1,-1],[-2,-1],[-3,-2],[1,1],[2,1],[3,2]])
+    #X = np.array([[-1,-1],[-2,-1],[-3,-2],[1,1],[2,1],[3,2]])
     kommunData = SCBDB.kommunToData('kommuner.txt')
-    print(kommunData)
+    #print(kommunData)
     kommunist = np.array(kommunData).astype(np.float)
-    print(kommunist)
+    #print(kommunist)
     #classes
-    Y = np.array([1,1,1,2,2,2])
+    #Y = np.array([1,1,1,2,2,2])
     klasser = np.array([1261,1266,1276,1280,1281])
-    print(klasser)
+    #print(klasser)
     clf = GaussianNB()
     GaussianNB(priors=None)
     #clf.fit(X,Y)
     clf.fit(kommunist,klasser)
     #test data
-    testKommun = SCBDB.SCBData("1380")
+    testKommun = SCBDB.SCBData("1381")
     print(clf.predict(testKommun.featureList))
 
     #print(clf.predict([[1,-1]]))
