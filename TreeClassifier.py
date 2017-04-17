26a9e928abf750f2e8b5eece56a92e4106192c8e
Benny Feng <bennyfeng96@hotmail.com>
2017-04-17 16:10:35 +0200

    added mapper for kommuncodes

--- TreeClassifier.py
+++ TreeClassifier.py
@@ -1,19 +1,20 @@
 import SCBDB
 import time
 from sklearn import tree
+import numpy
 
 start = time.time()
 
 
-kommunlist = SCBDB.kommunToData('kommuner.txt')
+kommunlist = numpy.array(SCBDB.kommunToData('kommuner.txt'))
 
 
 clf = tree.DecisionTreeClassifier()
-klasser = [1261,1266,1276,1280,1281]
+klasser = numpy.array([1261,1266,1276,1280,1281])
 clf = clf.fit(kommunlist,klasser)
 
 test = SCBDB.SCBData(1272).featureList
-clf.predict(test)
+print(clf.predict(test))
 
 
 
