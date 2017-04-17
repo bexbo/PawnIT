9d5693a8ac8e55a595a6a3dc69badaf024b409ab
Robert Bojs <rbojs@kth.se>
2017-04-17 16:32:23 +0200

    naive_bayes added kommun mapper

--- MLClassifier.py
+++ MLClassifier.py
@@ -1,8 +1,8 @@
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 import SCBDB as SCBDB
-
-
+import kommun_mapper as km
+import time
 def naive_bayes():
     #training data
     #X = np.array([[-1,-1],[-2,-1],[-3,-2],[1,1],[2,1],[3,2]])
@@ -19,12 +19,16 @@ def naive_bayes():
     #clf.fit(X,Y)
     clf.fit(kommunist,klasser)
     #test data
-    testKommun = SCBDB.SCBData("1381")
-    print(clf.predict(testKommun.featureList))
+    testKommun = SCBDB.SCBData(1381)
+    predict = clf.predict(testKommun.featureList)
+    predict = str(predict[0])
+    print(km.kommunMap[predict])
 
     #print(clf.predict([[1,-1]]))
     #clf_pf = GaussianNB()
     #clf_pf.partial_fit(X,Y,np.unique(Y))
     #print(clf_pf.predict([[-0.8,-1]]))
 
+start = time.time()
 naive_bayes()
+print(time.time()-start)
