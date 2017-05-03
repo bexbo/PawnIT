fb7a612dd97edf892ee7cbfb24320584ff7cb6d0
Robert Bojs <rbojs@kth.se>
2017-05-03 17:56:03 +0200

    added confusion matrix

--- MLClassifier.py
+++ MLClassifier.py
@@ -7,7 +7,16 @@ import shelve
 from matplotlib import pyplot as plt
 from matplotlib import colors
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+import itertools
+def random_forest():
+    X = ""
+    Y = ""
 
+    clf = RandomForestClassifier(n_estimators=10)
+    clf.fit(X,Y)
+    print("")
 
 def naive_bayes():
     #training data
@@ -55,6 +64,8 @@ def naive_bayes():
     predict = clf.predict(X_test)
     print(predict)
     print(clf.score(X_test,Y_test))
+    cnf_matrix = confusion_matrix(Y_test, predict)
+    class_names = [1,2,3]
     #print(clf.predict([[1,-1]]))
     #clf_pf = GaussianNB()
     #clf_pf.partial_fit(X,Y,np.unique(Y))
@@ -67,10 +78,48 @@ def naive_bayes():
     #ax.set_xlabel('$x$')
     #ax.set_ylabel('$y$')
     plt.plot(X_test,Y_test,'bo',label="woot")
+
+    np.set_printoptions(precision=2)
+
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+
     plt.show()
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
 
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
 
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 start = time.time()
 naive_bayes()
