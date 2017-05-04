c586efbbfe4ba5d71a86f959bffc49fd2908f373
Robert Bojs <robert.bojs@gmail.com>
2017-05-04 11:16:09 +0200

    made aggregate into function

--- CRMAggregate.py
+++ CRMAggregate.py
@@ -2,38 +2,39 @@ import shelve
 import pyodbc
 import CRMDB
 
-DBurl = 'DRIVER={SQL Server Native Client 10.0};SERVER=localhost;DATABASE=CustomerDATA;UID=kth;PWD=pass'
-cnxn = pyodbc.connect(DBurl)
-cursor = cnxn.cursor()
 
-cursor.execute('SELECT DISTINCT ZIP FROM [CustomerDATA].[dbo].[Company]')
-zips = cursor.fetchall()
+def createCRMDATA():
+    DBurl = 'DRIVER={SQL Server Native Client 10.0};SERVER=localhost;DATABASE=CustomerDATA;UID=kth;PWD=pass'
+    cnxn = pyodbc.connect(DBurl)
+    cursor = cnxn.cursor()
 
-#CRM = shelve.open('CRMData')
-kommun = shelve.open('postnummerToKommun')
-kommundata=shelve.open('kommundata')
-i=0
-for zip in zips:
-    zip = str(zip)
-    zip = zip.replace("'","").replace("(","").replace(")","").replace(" ","").replace(",","")
+    cursor.execute('SELECT DISTINCT zip FROM [CustomerDATA].[dbo].[Contact] as a join [CustomerDATA].[dbo].[SFA_Opportunity] on a.Contact_Id= [CustomerDATA].[dbo].[SFA_Opportunity].Contact_Id;')
+    zips = cursor.fetchall()
+
+    CRM = shelve.open('CRMData')
+    kommun = shelve.open('postnummerToKommun')
+    kommundata=shelve.open('kommundata')
+    i=0
+    for zip in zips:
+        zip = str(zip)
+        zip = zip.replace("'","").replace("(","").replace(")","").replace(" ","").replace(",","")
 
 
-    if zip.isdigit():
-        try:
-            kommun[zip] 
-            crm = CRMDB.CRMDB(zip)
-            crm.createDict()
-            d = crm.dictList
-            if d ==[]:
-                print(zip)
-           # print(d)
+        if zip.isdigit():
+            try:
+                print(kommun[zip])
 
-        except:
+                crm = CRMDB.CRMDB(zip)
+                crm.createDict()
+                d = crm.dictList
+                if d !=[]:
+                    print(zip)
 
-            i = i + 1
+                    CRM[kommun[zip]] = CRM[kommun[zip]].extend(d)
+               # print(d)
 
-print(len(zips))
-print(i)
+            except:
+                i = i + 1
 
 
 
