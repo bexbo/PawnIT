8fc2197679d8e79e31739f68b4da30409b52e7ed
Benny Feng <bennyfeng96@hotmail.com>
2017-05-04 14:45:54 +0200

    added success rates for kommun

--- CRMAggregate.py
+++ CRMAggregate.py
@@ -52,14 +52,65 @@ def createCRMDATA():
                 i = i + 1
 
 
-# crm = shelve.open('CRMData')
+crm = shelve.open('CRMData')
 # print(len(crm))
 # for thing in crm:
 #     print(thing)
 #     print(crm[thing])
 
+ilist=[]
+jlist=[]
+klist =[]
+upper = 0
+middle = 0
+lower = 0
+ulimit = 0.4
+mlimit = 0.2
+entrylimit = 4
+for kommun in crm:
+    i = 0
+    j = 0
+    k = 0
+    for postal in crm[kommun]:
 
-createCRMDATA()
+        for sale in postal:
+
+            k =k+1
+            if sale['a.[Status] ']==8:
+                i = i+1
+            elif sale['a.[Status] ']!=8:
+                j = j+1
+
+   # print('\n')
+
+
+    if k >= entrylimit:
+        if j == 0:
+            ilist.append(1)
+            upper = upper +1
+        elif i == 0:
+            lower = lower + 1
+            ilist.append(0)
+        else:
+            q = i/(i+j)
+            ilist.append(q)
+            if q >= ulimit:
+                upper =upper+1
+            elif q<ulimit and q>=mlimit:
+                middle=middle+1
+            else:
+                lower = lower+1
+
+        klist.append(k)
+  #  jlist.append(j)
+
+ilist.sort()
+print(len(ilist),list(reversed(ilist)))
+print(len(klist),klist)
+print('upper: ', upper,' middle: ',middle,' lower: ',lower)
+#print(jlist)
+       # print(sale['a.[Status] '])
+#createCRMDATA()
     #
     # crm = CRMDB.CRMDB(zip)
     #
